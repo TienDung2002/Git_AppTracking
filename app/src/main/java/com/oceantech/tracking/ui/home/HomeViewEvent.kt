0370adb0b35437695bfc593a05c506cd1b938dd7
TienDung2002 <101706065+TienDung2002@users.noreply.github.com>
2023-12-18 15:50:09 +0700

    first commit
    first commit

--- app/src/main/java/com/oceantech/tracking/ui/home/HomeViewEvent.kt
+++
@@ -1,8 +0,0 @@
-package com.oceantech.tracking.ui.home
-
-import com.oceantech.tracking.core.NimpeViewEvents
-
-sealed class HomeViewEvent:NimpeViewEvents{
-    object ResetLanguege:HomeViewEvent()
-    object SaveFeedback:HomeViewEvent()
-}
--- app/src/main/java/com/oceantech/tracking/ui/security/SecurityViewEvent.kt
+++
@@ -1,8 +0,0 @@
-package com.oceantech.tracking.ui.security
-
-import com.oceantech.tracking.core.NimpeViewEvents
-
-sealed class SecurityViewEvent:NimpeViewEvents {
-    object ReturnSigninEvent:SecurityViewEvent()
-    object ReturnResetpassEvent:SecurityViewEvent()
-}
