58cf8e23012de04dad3f379e7644c2c287af3a7c
TienDung2002 <101706065+TienDung2002@users.noreply.github.com>
2023-12-20 18:44:33 +0700

    update
    design login fragment

--- app/src/main/java/com/example/apptracking/ui/main_screen/MainActivity.kt
+++ app/src/main/java/com/example/apptracking/ui/main_screen/MainActivity.kt
@@ -1,11 +1,17 @@
-package com.example.apptracking
+package com.example.apptracking.ui.main_screen
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.apptracking.R
+import com.example.apptracking.ui.login.LoginFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.rootContainer, LoginFragment())
+            .commit()
     }
 }
